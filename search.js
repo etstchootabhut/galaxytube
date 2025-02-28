4b4bbdfb56ec54ef1f8d42c20279c52703f235b8
etstchootabhut <lostner467@gmail.com>
2025-02-28 13:57:51 +0530

    Add files via upload

--- search.js
+++ search.js
@@ -164,4 +164,9 @@ searchInput.addEventListener("input", searchItems);
     });
   });
 
+  document.addEventListener("fullscreenchange", () => {
+    if (document.fullscreenElement) {
+      screen.orientation.lock("landscape").catch(err => console.log(err));
+    }
+  });
   
